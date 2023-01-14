cf216644763db1195342c96f04d64ae3581694ff
Ebele <ebele.omanukwue@telus.com>
2023-01-13 18:49:31 -0700

    implement hook state functions and stack gui works

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,12 +6,23 @@ const useVisualMode = function(initial) {
   const [history, setHistory] = useState([initial]);
   const [mode, setMode] = useState(initial);
 
-  const transition = function(newMode, replace) {
+  const transition = (newMode, replace) => {
+    setMode(newMode);
+    setHistory(prev => ([...prev, newMode]));
+    if (replace) {
+      setHistory(history);
+    }
   };
 
-  const back = function() {
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history];
+      newHistory.pop();
+      const lastItemIndex = newHistory.length - 1;
+      setMode(newHistory[lastItemIndex]);
+      setHistory(newHistory);
+    }
   };
-
   // Don't forget this -> history is needed here
   return { mode, transition, back, history };
 };
